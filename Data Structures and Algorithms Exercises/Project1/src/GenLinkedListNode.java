3778ee2be4e71ec65129c20a07b0942d70cd9d0e
jsb220001 <113917800+jsb220001@users.noreply.github.com>
2024-06-12 13:07:28 -0500

    Add files via upload

---
+++ Data Structures and Algorithms Exercises/Project1/src/GenLinkedListNode.java
@@ -0,0 +1,23 @@
+/*
+
+//Creates and keeps track of GenLinkedListNode info
+
+import java.util.*;
+import java.io.*;
+
+public class GenLinkedListNode<E>
+{
+	public E contents;
+	public GenLinkedListNode<E> next;
+
+	public GenLinkedListNode(E e) 
+	{
+    	contents=e;
+    	next=null;
+	}
+}
+*/
+
+
+
+
